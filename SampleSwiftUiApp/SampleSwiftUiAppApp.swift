1ae5633914a6658672468406f52e892c47796a03
krishna <venkatakrishna.v@narayanagroup.com>
2022-07-14 18:50:26 +0530

    Initial Commit

---
+++ SampleSwiftUiApp/SampleSwiftUiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwiftUiAppApp.swift
+//  SampleSwiftUiApp
+//
+//  Created by Vasamsetti Pavana Venkata Krishna  on 14/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwiftUiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
