cab0a0a3ceef1351facd918f5446617ece8d6ecc
krishna <venkatakrishna.v@narayanagroup.com>
2022-08-04 16:35:56 +0530

    Added some ui for testing

--- SampleSwiftUiApp/ContentView.swift
+++ SampleSwiftUiApp/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedColor: Color = Color.red
+    @State var showColorPicker: Bool = false
+    
+    @State var showDatePicker: Bool = false
+    @State var selectedDate: Date = Date()
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+        Text("Hello, Krishna!\(selectedDate)")
             .padding()
+            .foregroundColor(selectedColor)
+            .background(Color.yellow)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .navigationTitle("Sample Swift UI App")
+            .navigationBarHidden(false)
+            .navigationViewStyle(.stack)
+        }
+        Button("Click Me") {
+            showColorPicker = !showColorPicker
+            showDatePicker = !showDatePicker
+        }
+        if(showColorPicker) {
+            ColorPicker("Selection", selection: $selectedColor)
+        }
+        if(showDatePicker) {
+            DatePicker(selection: $selectedDate, label: { Text("Date") })
+                        .datePickerStyle(WheelDatePickerStyle())
+        }
+        
     }
 }
 
